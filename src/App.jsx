35fad858806e24f13754a48866e253d9cac5134e
vtoan <vtoan@vtoan.local>
2021-09-28 10:20:14 +0700

    forgotpassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 
 const App = () => {
   return (
+    
     <Router>
       <Switch>
+      <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/forgot-password">
+          <ForgotPassword/>
+        </Route>
         <Route path="/">
           <Home />
         </Route>
+        
       </Switch>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,49 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 // import NavBarSeller from "../components/NavBarSeller";
 
-const SignUpSeller = () => {
+const Login = () => {
   return (
     <div>
       {/* <NavBarSeller /> */}
       <div className="flex h-screen bg-white mt-36 mx-20">
-        {/* LEFT */}
-        <div className="bg-cover w-1/2 text-center px-20">
-          <h3 className="text-2xl font-bold text-gray-900 p-10">
-            Ban hang chuyen nghiep
-          </h3>
-          <p className="text-gray-400 p-5">
-            Quản lý shop của bạn một cách hiệu quả hơn trên Shop Air với Shop
-            Air - Kênh Người bán
-          </p>
-          <img
-            src="https://deo.shopeemobile.com/shopee/shopee-seller-live-sg/rootpages/static/modules/account/image/login-img.9347138.png"
-            alt=""
-          />
-        </div>
+        
         {/* RIGHT */}
-        <div className="form-sign-up-seller w-1/2 px-20">
+        <div className="form-sign-up-center w-100 mx-auto my-20">
           <div className="form">
-            <h3 className="text-2xl font-bold text-gray-900 p-10 text-center">
-              Dang ky vao Kenh Nguoi Ban
-            </h3>
-            <input
-              type="text"
-              className="w-full p-3 shadow m-2"
-              name=""
-              id=""
-              placeholder="Nhap email..."
-            />
-            <input
-              type="text"
-              className="w-full p-3 shadow m-2"
-              name=""
-              id=""
-              placeholder="Nhap password..."
-            />
-            <button className="w-full m-2 p-3 bg-main text-white rounded">
-              Dang Ky
-            </button>
+              <h3 className="text-2xl font-bold text-gray-900 p-10 text-center">
+                Đăng nhập Kênh Người Bán
+              </h3>
+              <input
+                type="text"
+                className="w-full p-3 shadow m-2"
+                name=""
+                id=""
+                placeholder="Email..."
+              />
+              <input
+                type="text"
+                className="w-full p-3 shadow m-2"
+                name=""
+                id=""
+                placeholder="password..."
+              />
+              <div className="flex">
+              <input type="checkbox" name="" id="" className="w-5 h-5 p-3 shadow m-2"
+                />
+              <p className="text-sm text-gray-400 py-2" > Remember Me</p>
+              </div>
+              <button className="w-full m-2 p-3 bg-main text-white rounded">Đăng Nhập</button>
+
+              <Link className="text-sm text-gray-400 p-3" to="/forgot-password" >Lost Your Password?</Link>
+
           </div>
         </div>
       </div>
@@ -52,4 +46,4 @@ const SignUpSeller = () => {
   );
 };
 
-export default SignUpSeller;
+export default Login;
